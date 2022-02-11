29bbb465fa58d6b559913f4a619ba5cc20bddd87
Peter Tsivinsky <creatsiv@gmail.com>
2022-02-11 12:11:07 +0300

    feat: added static components

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,5 @@
+const Cart = (props) => {
+  <div>Cart</div>;
+};
+
+export default Cart;
---
+++ src/components/Meals/MealItem/MealItem.js
@@ -0,0 +1,23 @@
+import classes from "./MealItem.module.css";
+import MealItemForm from "./MealItemForm";
+
+const MealItem = (props) => {
+  const price = `$${props.price.toFixed(2)}`;
+
+  const addToCartHandler = () => {};
+
+  return (
+    <li className={classes.meal}>
+      <div>
+        <h3>{props.name}</h3>
+        <div className={classes.desc}>{props.desc}</div>
+        <div className={classes.price}>{price}</div>
+      </div>
+      <div>
+        <MealItemForm id={props.id} onAddToCart={addToCartHandler} />
+      </div>
+    </li>
+  );
+};
+
+export default MealItem;
