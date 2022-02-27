7b751cbd753198c60ef5d5a2e078cd6bc96d6b46
Aleksandr <sasha_yudin1998@mail.ru>
2022-02-27 21:36:26 +0300

    test commit

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -18,6 +18,7 @@ const Cart = (props) => {
   const cartItemAddHandler = (item) => {
     cartCtx.addItem({ ...item, amount: 1 });
   };
+  
 
   const cartItemRemoveHandler = (id) => {
     cartCtx.removeItem(id);
